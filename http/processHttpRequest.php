19d929b481561afdadad54fd12cf6d7ca4525506
akash r asli <ara59@afsconnect1.njit.edu>
2017-12-06 01:50:36 -0500

    Autoloading file

---
+++ http/processHttpRequest.php
@@ -0,0 +1,12 @@
+<?php
+namespace http;
+
+class processHttpRequest
+{
+    //this is the main function of the program to calculate the response to a get or post request
+    public static function createHttpResponse()
+    {
+        print("Autoloading processHttpRequest::createHttpResponse() ");
+    }
+}
+?>
